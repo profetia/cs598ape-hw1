87c372a4e82464bf8ca7fc0479a29658fcde0e02
Linshu Yang <linshuy2@illinois.edu>
2026-09-11 21:36:20 -0500

    Parallel with OpenMP

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 FUNC := icpx
 copt := -c 
 OBJ_DIR := ./bin/
-FLAGS := -O3 -march=native -flto -lm -g -Werror
+FLAGS := -O3 -march=native -flto -lm -g -Werror -qopenmp
 
 CPP_FILES := $(wildcard src/*.cpp)
 OBJ_FILES := $(addprefix $(OBJ_DIR),$(notdir $(CPP_FILES:.cpp=.obj)))
--- main.cpp
+++ main.cpp
@@ -46,6 +46,7 @@ void set(int i, int j, unsigned char r, unsigned char g, unsigned char b){
 }
 
 void refresh(Autonoma* c){
+   #pragma omp parallel for schedule(guided)
    for(int n = 0; n<H*W; ++n) 
    { 
       Vector ra = c->camera.forward+((double)(n%W)/W-.5)*((c->camera.right))+(.5-(double)(n/W)/H)*((c->camera.up));
