18c469b08e9928e1c3bb50ca7ea79a2e0e9c6aa3
Linshu Yang <linshuy2@illinois.edu>
2026-09-16 17:29:39 -0500

    Add script for comparing outputs

---
+++ imagediff.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import glob
+import sys
+
+
+def read_word(f):
+    c = f.read(1)
+    while c in b" \t\r\n":
+        c = f.read(1)
+
+    word = b""
+    while c not in b" \t\r\n":
+        word += c
+        c = f.read(1)
+    return word
+
+
+def read_ppm(filename):
+    f = open(filename, "rb")
+    width = int(read_word(f))
+    height = int(read_word(f))
+    pixels = f.read()
+    f.close()
+    return width, height, pixels
+
+
+left = sorted(glob.glob(sys.argv[1]))
+right = sorted(glob.glob(sys.argv[2]))
+
+different = 0
+total = 0
+largest = 0
+
+for left_name, right_name in zip(left, right):
+    left_width, left_height, left_pixels = read_ppm(left_name)
+    right_width, right_height, right_pixels = read_ppm(right_name)
+
+    for i in range(0, len(left_pixels), 3):
+        red = abs(left_pixels[i] - right_pixels[i])
+        green = abs(left_pixels[i + 1] - right_pixels[i + 1])
+        blue = abs(left_pixels[i + 2] - right_pixels[i + 2])
+        difference = max(red, green, blue)
+        if difference:
+            different += 1
+        largest = max(largest, difference)
+
+    total += left_width * left_height
+
+print("%d/%d pixels differ" % (different, total))
+print("maximum absolute difference: %d/255" % largest)
