2344db3bd9ef725be8677d258e355b5f80b10199
Linshu Yang <linshuy2@illinois.edu>
2026-09-16 21:16:13 -0500

    Use dynamic OpenMP scheduling for ablation

--- main.cpp
+++ main.cpp
@@ -41,7 +41,7 @@ void set(int i, int j, unsigned char r, unsigned char g, unsigned char b) {
 }
 
 void refresh(Autonoma *c) {
-#pragma omp parallel for schedule(static)
+#pragma omp parallel for schedule(dynamic)
   for (int n = 0; n < H * W; ++n) {
     Vector ra = c->camera.forward +
                 ((double)(n % W) / W - .5) * ((c->camera.right)) +
